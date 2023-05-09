2e3e4e8b721fee8908675c754df10873bc2ed73a
leejaejoseph <leejaejoseph@gmail.com>
2023-05-08 18:17:14 -0700

    Finished learning Quoting, Shortcutting, Printing, and Eclipse navigating

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("Hello World");
+		System.out.println("Hello World");
+		System.out.print("Uncollapses with next line \\n or System.out.println\n");
+		System.out.println("\t Tabs with \\t ");
+		System.out.println("Same as JS \\ using for \" \\, \\n \\t");
+		// comment out with double // or /**/
+		
+		/*
+		 * commented
+		 * multiple
+		 * line
+		 */
 	}
 }
