7cf9142155b028e87846340321984269e6abd1aa
leejaejoseph <leejaejoseph@gmail.com>
2023-05-10 14:08:54 -0700

    Completed constructors.

---
+++ src/ClassRoom.java
@@ -0,0 +1,13 @@
+
+public class ClassRoom {
+	String name;
+	String course;
+	int id;
+	
+	ClassRoom(String name, String course, int id){
+		this.name = name;
+		this.course = course;
+		this.id = id;
+	}
+	
+}
